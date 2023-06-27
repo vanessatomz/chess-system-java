1cb434bdf607e71cd1e4c392ad4f5ae12a88fb7e
Vanessa Tomaz <vanessatomaz246@gmail.com>
2023-06-27 15:22:00 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,11 +25,15 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece,new ChessPosition(column,row).toPosition());
+    }
+
     // Iniciar a partida colocando as peças no tabuleiro
     private void initialSetup(){
-        board.placePiece(new Rook(board,Color.WHITE),new Position(2,1));
-        board.placePiece(new King(board,Color.BLACK),new Position(0,4));
-        board.placePiece(new King(board,Color.BLACK),new Position(2,0));
+        placeNewPiece('b',6, new Rook(board,Color.WHITE));
+        placeNewPiece('e',8,new King(board,Color.BLACK));
+        placeNewPiece('e',1,new King(board,Color.BLACK));
 
     }
 
