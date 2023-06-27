567800639b97c4f3f35b42c3a68d258797c8d91a
Vanessa Tomaz <vanessatomaz246@gmail.com>
2023-06-27 11:16:00 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
 
 public class Program {
     public static void main(String[] args) {
-        Position position = new Position(3,2);
-        System.out.println(position);
+        Board board = new Board(8,8);
+
+
     }
 }
