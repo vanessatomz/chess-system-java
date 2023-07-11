86375975b3f31511a6eae3adb2a1b533e08b2335
Vanessa Tomaz <vanessatomaz246@gmail.com>
2023-07-11 16:30:19 -0300

    Checkmate Logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while(true){
+        while(!chessMatch.getCheckMate()){
             try{
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -46,6 +46,7 @@ public class Program {
             }
 
         }
-
+        UI.clearScreen();
+        UI.printMatch(chessMatch,captured);
     }
 }
