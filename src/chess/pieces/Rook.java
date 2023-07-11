57527a675261e2c2973cbec3cdc71f669e61c66d
Vanessa Tomaz <vanessatomaz246@gmail.com>
2023-07-11 11:29:43 -0300

    Possible moves of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -14,5 +14,10 @@ public class Rook extends ChessPiece {
     public String toString() {
         return "♖";
     }
+
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+    }
 }
 
