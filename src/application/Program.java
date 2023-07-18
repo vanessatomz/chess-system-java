95c5e63efe9a735cc24853dca4db10c4e14664bb
Vanessa Tomaz <vanessatomaz246@gmail.com>
2023-07-18 14:16:14 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,9 +37,17 @@ public class Program {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+
                 if (capturedPiece != null){
                     captured.add(capturedPiece);
                 }
+
+                if(chessMatch.getPromoted() != null){
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
+
             }catch (ChessException | InputMismatchException e ){
                 System.out.println(e.getMessage());
                 sc.nextLine();
